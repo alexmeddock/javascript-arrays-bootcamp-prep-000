b506df70858a0e7cd5fa8179d711eb341b3a908f
Alex Meddock <alexmeddock@gmail.com>
2019-09-14 16:31:00 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,32 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+function destructivelyChocolateBars(name){
+    return chocolateBars.push(name)
+}
+
+function destructivelyPrependKitten(name){
+    return chocolateBars.unshift(name)
+}
+
+function destructivelyRemoveLastChocolateBars(){
+    return chocolateBars.pop()
+}
+
+function destructivelyRemoveFirstChocolateBars(){
+    return chocolateBars.shift()
+}
+
+function appendChocolateBars(name){
+    return [...chocolateBars, name]
+}
+
+function prependChocolateBars(name){
+    return [name, ...chocolateBars]
+}
+
+function removeLastChocolateBars(){
+    return chocolateBars.slice(0,ChocolateBars.length-1)
+}
+
+function removeFirstChocolateBars(){
+    return chocolateBars.slice(1)
+}
