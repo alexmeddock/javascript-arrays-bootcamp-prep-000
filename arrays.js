7388871eb1df123ea2ae3e11bd39a5f80b481daa
Alex Meddock <alexmeddock@gmail.com>
2019-09-14 16:27:50 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,1 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
-var hockey = ["Pens", "bruins", "avalanche"];
-function destructivelyAppendHockey(name){
-    return hockey.push(name)
-}
